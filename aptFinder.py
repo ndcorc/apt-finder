4812186cce6ef96804d552afc6e8bbd84387519a
Nolan Corcoran <nolan@identity.utexas.edu>
2017-08-10 22:36:07 -0500

    3rd party api workaround

--- aptFinder.py
+++ aptFinder.py
@@ -1,9 +1,11 @@
 import bs4
-import urllib2 as url
+import urllib2
 
 class AptFinder:
     def __init__(self):
-        self.page = url.urlopen('http://www.apartmentfinder.com/Texas/Austin-Apartments/Azul-Lakeshore-Apartments')
+        self.url = 'http://www.apartmentfinder.com/Texas/Austin-Apartments/Azul-Lakeshore-Apartments'
+        self.req = urllib2.Request(self.url, headers={'User-Agent' : "Magic Browser"})
+        self.page = urllib2.urlopen(self.req)
         self.soup = bs4.BeautifulSoup(self.page.read(), 'html.parser')
         self.models = ["1A", "1C", "1F", "2A", "2B", "2C", "2D"]
         self.available = []
--- aptsDotCom.py
+++ aptsDotCom.py
@@ -1,9 +1,11 @@
 import bs4
-import urllib2 as url
+import urllib2
 
 class AptsDotCom:
     def __init__(self):
-        self.page = url.urlopen('https://www.apartments.com/azul-lakeshore-austin-tx/2lldd29/')
+        self.url = 'https://www.apartments.com/azul-lakeshore-austin-tx/2lldd29/'
+        self.req = urllib2.Request(self.url, headers={'User-Agent' : "Magic Browser"})
+        self.page = urllib2.urlopen(self.req)
         self.soup = bs4.BeautifulSoup(self.page.read(), 'html.parser')
         self.models = ["1A", "1C", "1F", "2A", "2B", "2C", "2D"]
         self.available = []
