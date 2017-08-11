f0f15ac4c4868c2f93142d8fd12d29db4db92ac4
Nolan Corcoran <nolan@identity.utexas.edu>
2017-08-10 23:29:41 -0500

    request module instead

--- aptFinder.py
+++ aptFinder.py
@@ -1,5 +1,6 @@
 import bs4
 import urllib2
+import requests
 
 headers = {
     'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/60.0.3112.90 Safari/537.11',
@@ -13,10 +14,8 @@ headers = {
 class AptFinder:
     def __init__(self):
         self.url = 'http://www.apartmentfinder.com/Texas/Austin-Apartments/Azul-Lakeshore-Apartments'
-
-        self.req = urllib2.Request(self.url, headers=headers)
-        self.page = urllib2.urlopen(self.req)
-        self.soup = bs4.BeautifulSoup(self.page.read(), 'html.parser')
+        self.page = requests.get(self.url).content
+        self.soup = bs4.BeautifulSoup(self.page, 'html.parser')
         self.models = ["1A", "1C", "1F", "2A", "2B", "2C", "2D"]
         self.available = []
 
