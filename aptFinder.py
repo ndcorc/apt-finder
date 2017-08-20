e39ee37dbe67b7372ff625cdc032cd097c46b45f
Nolan Corcoran <nolan@identity.utexas.edu>
2017-08-20 16:32:13 -0500

    shortened

--- aptFinder.py
+++ aptFinder.py
@@ -2,15 +2,6 @@ import bs4
 import urllib2
 import requests
 
-headers = {
-    'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/60.0.3112.90 Safari/537.11',
-    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-    'Accept-Charset': 'ISO-8859-1,utf-8;q=0.7,*;q=0.3',
-    'Accept-Encoding': 'none',
-    'Accept-Language': 'en-US,en;q=0.8',
-    'Connection': 'keep-alive'
-}
-
 class AptFinder:
     def __init__(self):
         self.url = 'http://www.apartmentfinder.com/Texas/Austin-Apartments/Azul-Lakeshore-Apartments'
